a2bded3e86f9477a29b6efc1c756ae5c167eb1a0
pr0kk <105685714+pr0kk@users.noreply.github.com>
2022-06-05 17:47:06 +0900

    Implement Authentication and Authorization using JWT
    1-Add spring security dependency
    + Spring-boot-starter-security
    
    2-JWT does not ship as a part of spring security, so have to add the java-jwt dependency
    + java-jwt
    
    3-Spring Boot ships with an automatically configured security module that must be disabled, as we will be implementing our own.
    This must be done in the Application class.
    -> Add Bean BCryptPasswordEncoder
    
    4-Create password for the user
    
    5-Once that is disabled, you will need to implement 4 classes
    + a subclass of UsernamePasswordAuthenticationFilter for taking the username and password from a login request and logging in. This, upon successful authentication, should hand back a valid JWT in the Authorization header
    -> JWTAuthenticationFilter
    P/s: Add SecurityConstants.java
    This class contains the literal constants that are used in the JWTAuthenticationFilter class.
    
    + a subclass of BasicAuthenticationFilter.
    -> JWTAuthenticationVerficationFilter
    
    + an implementation of the UserDetailsService interface. This should take a username and return a userdetails User instance with the user's username and hashed password.
    -> UserDetailsServiceImpl
    
    + a subclass of WebSecurityConfigurerAdapter. This should attach your user details service implementation to Spring's AuthenticationManager. It also handles session management and what endpoints are secured. For us, we manage the session so session management should be disabled. Your filters should be added to the authentication chain and every endpoint but 1 should have security required. The one that should not is the one responsible for creating new users.
    -> WebSecurityConfiguration

--- starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -1,11 +1,23 @@
 package com.example.demo.model.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class CreateUserRequest {
 
 	@JsonProperty
 	private String username;
+
+	@JsonProperty
+	private String password;
+
+	@JsonProperty
+	private String confirmPassword;
 
 	public String getUsername() {
 		return username;
