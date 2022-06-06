cff12325ba0c3f5a42277bcbbbb4dcc617242c9d
pr0kk <105685714+pr0kk@users.noreply.github.com>
2022-06-06 20:26:18 +0900

    Update UserDetailsServiceImpl.java to fix login error

--- starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
+++ starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.security;
 
-import com.example.demo.controllers.UserController;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import org.apache.logging.log4j.LogManager;
@@ -11,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Collections;
 
 @Service
@@ -22,11 +22,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private static final Logger log = LogManager.getLogger(UserDetailsServiceImpl.class);
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username){
         User user = userRepository.findByUsername(username);
         if (user == null) {
             log.error("[ERROR] Username Not Found Exception!!");
-            throw new UsernameNotFoundException(username);
+            throw new EntityNotFoundException(String.format("User with name: %s not found", username));
         }
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.emptyList());
     }
